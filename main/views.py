c51fb07a3fe1c1af9d5093561723e28046584908
Rhythm1821 <alamrawat1821@gmail.com>
2024-02-29 21:36:43 +0530

    worked on user profile

--- main/views.py
+++ main/views.py
@@ -17,7 +17,18 @@ def profile_list(request):
 def profile(request,pk):
     if request.user.is_authenticated:
         profile = Profile.objects.get(user_id=pk)
+        if request.method=='POST':
+            # Get current user
+            current_user_profile=request.user.profile
+            # Get form data
+            action = request.POST['follow']
+            # Decide to follow or unfollow
+            if action=='unfollow':
+                current_user_profile.follows.remove(profile)
+            elif action=='follow':
+                current_user_profile.follows.add(profile)
+            current_user_profile.save()
         return render(request,'profile.html',{'profile':profile})
     else:
-        messages.success(request,'You must be logged in to view this...')
+        messages.warning(request,'You must be logged in to view this...')
         return redirect('home')
